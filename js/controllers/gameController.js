4e166dad5feece2dfa30ae1d7b54ad7dc4aee576
Aaron Hill <aahi@microsoft.com>
2016-07-25 16:00:34 -0700

    improved user input

--- js/controllers/gameController.js
+++ js/controllers/gameController.js
@@ -1,10 +1,21 @@
 app.controller("gameController", ['$scope', function($scope){
-    $scope.player = {lastCommand: ""}
+    $scope.player = {
+        lastCommands: [], 
+        inv: []
+    }
     $scope.currInput = "..."
+    $scope.currOutput = "You find yourself in a dark room..."
+
+
+    $scope.textList = []
 
     $scope.submit = function(){
         if ($scope.currInput){
-            $scope.player.lastCommand = $scope.currInput;
+            //push the last input and output strings to keep track of them for later rendering
+            $scope.textList.push($scope.currInput)
+            $scope.textList.push($scope.currOutput)
+
+            $scope.player.lastCommands.push($scope.currInput);
             $scope.currInput = ""
         } 
     };
